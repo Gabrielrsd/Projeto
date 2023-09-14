47308a789a416a70951f6e79b6db206dea9c5337
Rsde7 <gabrielgomes_98@hotmail.com>
2023-09-14 16:00:08 -0300

    fixes

--- script.js
+++ script.js
@@ -10,11 +10,14 @@ function toggleMode() {
     // se tiver light mode, adicionar a imagem light
     img.setAttribute("src", "./assets/avatar-light.png")
 
-    img.setAttribute("alt", "Foto de Gabriel sério tomando café da manhã na Confeitaria Colombo")
+    img.setAttribute("alt", "Foto de Gabriel sério olhando para o lado")
   } else {
     // set tiver sem light mode, manter a imagem normal
     img.setAttribute("src", "./assets/avatar.png")
-    
-    img.setAttribute("alt", "Foto de Gabriel sério olhando para o lado")
+
+    img.setAttribute("alt","Foto de Gabriel sério tomando café da manhã na Confeitaria Colombo")
   }
 }
+
+
+
